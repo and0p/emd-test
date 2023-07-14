2dee01e2754a894aa248178f40f0807302335e69
Andrew Peterson <andrew@peterson.sh>
2023-07-14 14:30:18 -0400

    feat: cc validation api endpoint

--- backend/src/Controllers/CCValidator.ts
+++ backend/src/Controllers/CCValidator.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from 'express';
 
-const ValidateCC = (req: Request, res: Response) => {
+import { validateCreditCard } from '../Services/CCValidator';
 
+export const validateCreditCardController = (req: Request<{ creditCardNumber: string }>, res: Response) => {
+    try {
+        const cc = req.body.creditCardNumber;
+
+        // Ensure that the parameter passed in was a string
+        if (typeof cc !== 'string') {
+            res.status(400).send('Expected a string for creditCardNumber.');
+            return;
+        }
+    
+        // Check for valid formatting
+        const validationResult = validateCreditCard(cc);
+    
+        res.status(validationResult.valid ? 200 : 400).json(validationResult);
+        return;
+    } catch (err: any) {
+        res.status(500).send();
+        return;
+    }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import { validateCreditCardController } from './Controllers/CCValidator';
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.post('/api/validateCreditCard', validateCreditCardController);
 
 app.listen(port, () => {
   console.log(`EMD CC validator listening on port ${port}`)
