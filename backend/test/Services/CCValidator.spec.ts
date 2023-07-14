ba69dcb7988ca8c327f6198b95dd72d71b1c73a5
Andrew Peterson <andrew@peterson.sh>
2023-07-14 12:20:20 -0400

    chore: scaffolding backend

---
+++ backend/test/Services/CCValidator.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import 'mocha';
+
+// Could use named functions rather than lambdas here, easier debugging on larger projects, but keeping it simple
+describe('Credit Card Validation', () => {
+    describe('Success cases', () => {
+
+    });
+
+    describe('Failure cases', () => {
+
+    });
+});
